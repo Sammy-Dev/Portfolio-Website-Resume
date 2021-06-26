a4a82f02ba118111e641e9660773f8dabcf7a9f2
sammy <58030710+Sammy-Dev@users.noreply.github.com>
2021-06-26 22:43:48 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,57 @@
+//select DOM items
+const menuBtn = document.querySelector(".burgerButton");
+const menu = document.querySelector(".theMenu");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+//set Initial menu state
+let showMenu = false; 
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+        menuBtn.classList.add("close");
+        menu.classList.add('show');
+        menuNav.classList.add("show");
+        navItems.forEach(item => item.classList.add("show"));
+        
+        showMenu = true;
+    }else{
+        menuBtn.classList.remove("close");
+        menu.classList.remove("show");
+        menuNav.classList.remove("show");
+        navItems.forEach(item => item.classList.remove("show"));
+        showMenu = false;
+    }
+}
+
+///////////////////////////////////////
+var Clock;
+var i = 1;
+
+function Start(){
+    Clock = setInterval(Animation, 500);
+}
+
+function Animation(){
+    if(i == 0){
+        document.getElementById("underScore").innerHTML = " ";
+        i++;
+    }else if(i == 1){
+        document.getElementById("underScore").innerHTML = "_";
+        i--;
+    }
+} 
+
+/* //drop down project menu 
+function dropDown(description) {     //description specifies what description info is being toggled
+    var x = document.getElementById(description);
+
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+ */
